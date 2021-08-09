3927974de44363fb434339826003c8beb6568f76
Ben Wilson <85944118+BRWilson-UVA@users.noreply.github.com>
2021-08-09 09:18:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -160,5 +160,5 @@ app.layout = html.Div(
 
 port = int(os.environ.get("PORT", 8051))
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=port, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run_server(debug=True)
